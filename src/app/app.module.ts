ba3ba15d261b14923bd72f8ed66d759aca314f7e
Mauricio Arce <mauricio.arce.mat@gmail.com>
2018-02-23 20:30:30 -0400

    First libraries added and home menu implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
