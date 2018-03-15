130531b3d4741c7ea987b3c52996d667df1ed6f7
Griss Garcia <grissgarcia74@gmail.com>
2018-03-15 19:11:46 -0400

    orderrs list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -20,6 +21,7 @@ import { OrderService } from './services/order.service';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
     AppRoutingModule
     // ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
